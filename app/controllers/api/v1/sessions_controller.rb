c8ee55e5e82bb8bdf1e7ce951ee86764c37895ab
Arique <arique1104@gmail.com>
2025-04-19 13:00:00 -0700

    Adds session controller

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,19 @@
+class Api::V1::SessionsController < ApplicationController
+     def create
+        user = User.find_by(email: params[:email])
+        if user&.authenticate(params[:password])
+          token = encode_token({ user_id: user.id })
+          render json: { user: user, token: token }
+        else
+          render json: { error: 'Invalid email or password' }, status: :unauthorized
+        end
+      end
+
+      def show
+        if current_user
+          render json: { user: current_user }
+        else
+          render json: { error: "Not logged in" }, status: :unauthorized
+        end
+      end
+end
