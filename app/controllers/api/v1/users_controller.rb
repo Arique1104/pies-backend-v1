af31ae37db80f1c85d1eb57bb7076deed1524e4a
Arique <arique1104@gmail.com>
2025-04-19 13:00:13 -0700

    Adds Users Controller

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,21 @@
+class Api::V1::UsersController < ApplicationController
+    def ping
+        render json: { status: "UsersController is alive!" }
+    end
+    
+    def create
+    user = User.new(user_params)
+    if user.save
+      token = JWT.encode({ user_id: user.id }, Rails.application.secret_key_base)
+      render json: { user: user, token: token }, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
