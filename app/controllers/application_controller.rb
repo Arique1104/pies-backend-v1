4a55a18143333098a1d07be4fe687046931f9166
Arique <arique1104@gmail.com>
2025-04-19 12:57:02 -0700

    Adds JWT encode and decode, current user and authorize user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,28 @@
+# app/controllers/application_controller.rb
 class ApplicationController < ActionController::API
+  def encode_token(payload)
+    JWT.encode(payload, Rails.application.secret_key_base)
+  end
+
+  def decoded_token
+    auth_header = request.headers['Authorization']
+    return unless auth_header
+
+    token = auth_header.split(' ')[1]
+    begin
+      JWT.decode(token, Rails.application.secret_key_base, true, algorithm: 'HS256')[0]
+    rescue JWT::DecodeError
+      nil
+    end
+  end
+
+  def current_user
+    return unless decoded_token
+
+    @current_user ||= User.find_by(id: decoded_token['user_id'])
+  end
+
+  def authorize_user
+    render json: { error: 'Unauthorized' }, status: :unauthorized unless current_user
+  end
 end
