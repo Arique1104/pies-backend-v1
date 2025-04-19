e42cfc752b0f04bc06eca2d7fe5052cde3a6d203
Arique <arique1104@gmail.com>
2025-04-19 13:01:32 -0700

    updates format of routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,20 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  get "/controller-alive", to: "api/v1/users#ping"
 
   namespace :api do
     namespace :v1 do
-      resources :users, only: [] do
-        resources :pies_entries, only: [:index, :create]
+      resources :users, only: [:create], defaults: { format: :json } do
+        resources :pies_entries, only: [:index, :create] do
+          collection do
+            get :latest
+          end
+        end
       end
+
+      post "/login", to: "sessions#create"
+      get "/me", to: "sessions#show"
+
       resources :organizations, only: [:create, :index, :show]
       resources :memberships, only: [:create]
       resources :reflection_tips, only: [:index, :create, :destroy]
