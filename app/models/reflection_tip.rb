18de84126dce2f284dcaf239ab413e187e9c3ec8
Arique <arique1104@gmail.com>
2025-04-19 17:32:14 -0700

    Adds has many relationships for secure usability

--- app/models/reflection_tip.rb
+++ app/models/reflection_tip.rb
@@ -2,4 +2,8 @@ class ReflectionTip < ApplicationRecord
     validates :keyword, presence: true
     validates :category, presence: true, inclusion: { in: %w[physical intellectual emotional spiritual] }
     validates :tip, presence: true, uniqueness: true
+
+    has_many :tip_ratings, dependent: :destroy
+    has_many :favorite_tips, dependent: :destroy
+    has_many :favoriting_users, through: :favorite_tips, source: :user
 end
