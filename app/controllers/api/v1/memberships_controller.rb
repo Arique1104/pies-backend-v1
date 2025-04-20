9004b9997ef5618f038a454a8d5bff12c0a69ba3
Arique <arique1104@gmail.com>
2025-04-20 07:43:48 -0700

    Linter error

--- app/controllers/api/v1/memberships_controller.rb
+++ app/controllers/api/v1/memberships_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::MembershipsController < ApplicationController
-  before_action :authorize_manager_or_owner!, only [:create]
+  before_action :authorize_manager_or_owner!
 
   def create
     membership = Membership.new(membership_params)
