ebbf9de02173e21f1b48e0b35c1327e27821113e
Arique <arique1104@gmail.com>
2025-04-21 12:06:20 -0700

    Removes ping method from debugging

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,4 @@
 class Api::V1::UsersController < ApplicationController
-    def ping
-        render json: { status: "UsersController is alive!" }
-    end
 
     def create
     user = User.new(user_params)
