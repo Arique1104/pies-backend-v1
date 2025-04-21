e4b4a750b2053cc83635cccfb43fdc8c4ff30f8e
Arique <arique1104@gmail.com>
2025-04-21 12:26:30 -0700

    linter errors

--- config/routes.rb
+++ config/routes.rb
@@ -5,31 +5,31 @@ Rails.application.routes.draw do
     namespace :v1 do
       # 🔐 Authenticated user actions
       resources :users, only: [ :create ], defaults: { format: :json }
-      
+
       resources :pies_entries, only: [ :index, :create ] do
         collection do
           get :latest
         end
       end
-      
+
       resources :reflection_tips, only: [ :index ] do
         member do
           post :rate
           post :favorite
         end
-        
+
         collection do
-          get :favorites  
+          get :favorites
         end
       end
-        
+
         # 🔐 Auth
         post "/login", to: "sessions#create"
         get "/me", to: "sessions#show"
-        
+
         # 🛠 Product owner controls for managing tips
         resources :reflection_tips, only: [ :create, :destroy, :update, :show ]
-        
+
         resources :organizations, only: [ :create, :index, :show ]
         resources :memberships, only: [ :create ]
     end
