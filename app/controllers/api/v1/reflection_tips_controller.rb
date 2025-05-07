7a522013df29e63d32c9f026e8e7aaed5ae91699
Arique <arique1104@gmail.com>
2025-05-07 11:34:21 -0700

    migrating CRUD functionality from ReflectionTipsController to KeywordsController

--- app/controllers/api/v1/reflection_tips_controller.rb
+++ app/controllers/api/v1/reflection_tips_controller.rb
@@ -1,23 +1,23 @@
 class Api::V1::ReflectionTipsController < ApplicationController
-    def index
-        tips = ReflectionTip.all.order(:category, :keyword)
-        render json: tips
-    end
+    # def index
+    #     tips = ReflectionTip.all.order(:category, :keyword)
+    #     render json: tips
+    # end
 
-    def create
-        tip = ReflectionTip.new(tip_params)
-        if tip.save
-          render json: tip, status: :created
-        else
-          render json: { errors: tip.errors.full_messages }, status: :unprocessable_entity
-        end
-    end
+    # def create
+    #     tip = ReflectionTip.new(tip_params)
+    #     if tip.save
+    #       render json: tip, status: :created
+    #     else
+    #       render json: { errors: tip.errors.full_messages }, status: :unprocessable_entity
+    #     end
+    # end
 
-      def destroy
-        tip = ReflectionTip.find(params[:id])
-        tip.destroy
-        head :no_content
-      end
+      # def destroy
+      #   tip = ReflectionTip.find(params[:id])
+      #   tip.destroy
+      #   head :no_content
+      # end
 
       def rate
         tip = ReflectionTip.find(params[:id])
