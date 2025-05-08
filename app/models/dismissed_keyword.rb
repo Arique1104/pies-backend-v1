9a7a573fd6fbb52d27342455eca3a26c8c59f918
Arique <arique1104@gmail.com>
2025-05-08 11:32:29 -0700

    Adds model validation that example column exists

--- app/models/dismissed_keyword.rb
+++ app/models/dismissed_keyword.rb
@@ -1,5 +1,5 @@
 class DismissedKeyword < ApplicationRecord
-  validates :word, :category, presence: true
+  validates :word, :category, :example, presence: true
   validates :word, uniqueness: { scope: :category }
   validates :category, inclusion: { in: %w[physical intellectual emotional spiritual] }
 end
